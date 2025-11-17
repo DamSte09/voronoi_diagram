dcc13255bf4af97669060b0ee4ef53c155b1aebd
DamSte09 <damste09@gmail.com>
2025-11-17 19:12:40 +0100

    feat: calculating circle_center and new record of cc after removing arc

--- src/algorithms/circle.py
+++ src/algorithms/circle.py
@@ -1,13 +1,18 @@
 from src.structures.QE import EventsQueue
-from src.structures.DCEL import DCEL
+from src.structures.DCEL import DCEL, Vertex
 from src.structures.BST import Leaf, Node
-from funcs import successor,predecessor, remove_from_queue
+from funcs import successor,predecessor, remove_from_queue, circle_center
 
 def handle_circle_event(y: Leaf, root: Node, queue: EventsQueue, dcel: DCEL):
     left_leaf = predecessor(y)
     right_leaf = successor(y)
 
     root.remove_leaf(y)
+
+    A = left_leaf.centre
+    B = y.centre
+    C = right_leaf.centre
+
     queue.remove_circle_event(y)
     
     for neighbor in (left_leaf, right_leaf):
@@ -15,8 +20,22 @@ def handle_circle_event(y: Leaf, root: Node, queue: EventsQueue, dcel: DCEL):
             queue.remove_circle_event(neighbor)
             neighbor.circle_event = None
 
-    
     root.balance_tree()
+            
+    cc = circle_center(A, B, C)
+    
+    V_cc = Vertex(cc)
+    dcel.vertices.append(V_cc)
+    
+    dcel.add_halfedges()
+    
+
+    
+    
+    
+
+
+    
     
     
     
